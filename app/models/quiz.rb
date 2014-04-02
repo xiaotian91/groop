296908b5cb10d059540fd13608289bbc3e4f3e44
Carl Johnson <john3123@msu.edu>
2014-04-01 20:03:35 -0400

    more error checking and bug fixes

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,3 +1,12 @@
 class Quiz < ActiveRecord::Base
+  validates :one, presence: true
+  validates :two, presence: true
+  validates :three, presence: true
+  validates :four, presence: true
+  validates :five, presence: true
+  validates :six, presence: true
+  validates :seven, presence: true
+  validates :eight, presence: true
+  
   belongs_to :user
 end
