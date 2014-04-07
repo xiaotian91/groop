ccf2a403f823361161aa9870ad5614bb8b5e09d6
Carl Johnson <john3123@msu.edu>
2014-04-07 16:31:02 -0400

    commit matching done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
  
   def show
     @user = User.find(params[:id])
+    @users = User.all
     @registration = @user.registrations.new
   end
 
@@ -39,5 +40,42 @@ class UsersController < ApplicationController
 
   def logout
   end
+
+
+def computeCompat
+
+ for user in @users
+   compatScore = 100 
+
+  if @user.quiz.one!=user.quiz.one 
+     compatScore = compatScore - 5 
+  end
+  if @user.quiz.two!=user.quiz.two
+    compatScore = compatScore - 5
+  end
+  if @user.quiz.three!=user.quiz.three
+    compatScore = compatScore - 5 
+  end
+  if @user.quiz.three!=user.quiz.three
+    compatScore = compatScore - 5 
+  end
+  if @user.quiz.four!=user.quiz.four
+    compatScore = compatScore - 5 
+  end
+  if @user.quiz.five!=user.quiz.five
+   compatScore = compatScore - 5 
+  end
+  if @user.quiz.six!=user.quiz.six 
+    compatScore = compatScore - 5 
+  end
+  if @user.quiz.seven!=user.quiz.seven 
+    compatScore = compatScore - 5 
+  end
+  if @user.quiz.eight!=user.quiz.eight 
+    compatScore = compatScore - 5 
+  end
+ end
+end
+
 
 end
